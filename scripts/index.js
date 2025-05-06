a28e67627ee758b548c5545b34319e4da1dcb4a5
krwcw <clementoboy@gmail.com>
2025-05-06 16:28:53 +0300

    Коммит для ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,49 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardContainer = document.querySelector('.places__list');
+const addButton = document.querySelector('.profile__add-button');
 
-// @todo: DOM узлы
+// индекс, с которого начинается добавление новых карточек
+let currentIndex = 6;
 
-// @todo: Функция создания карточки
+// функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
 
-// @todo: Функция удаления карточки
+// функция создания карточек
+function createCard(cardData) { 
+    const cardFragment = cardTemplate.cloneNode(true);
+    const cardElement = cardFragment.querySelector('.card');
 
-// @todo: Вывести карточки на страницу
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = cardData.link
+
+    const cardName = cardElement.querySelector('.card__title');
+    cardName.textContent = cardData.name
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    // кнопка удаления карточки
+    deleteButton.addEventListener('click', () => {
+        deleteCard(cardElement) 
+    });
+
+    cardContainer.appendChild(cardElement);
+}
+
+// добавление карточек
+addButton.addEventListener('click', function addButtonAction() {
+
+    if (currentIndex < initialCards.length) {
+        createCard(initialCards[currentIndex])
+        currentIndex++;
+        if (currentIndex === initialCards.length) {
+            addButton.disabled = true;
+        }
+    }
+});
+
+// вывод первых 6 карточек
+for (let i = 0; i < 6; i++) {
+    createCard(initialCards[i]);
+}
